969cb1dbe844fa86c0211cdef4c9f374d3737b39
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2017-09-07 12:09:55 +0200

    Sketch idea of an exporting traversal in the XHTML case.

--- document.go
+++ document.go
@@ -5,13 +5,84 @@ package main
 
 type nodeType int
 
+const (
+	orderedListNode nodeType = iota
+	unorderedListNode
+	textNode
+	boldNode
+	italicsNode
+	underlinedNode
+	strikeNode
+	superscriptNode
+	subscriptNode
+	tableNode
+	tableRowNode
+	tableCellNode
+	heading1Node
+	heading2Node
+	heading3Node
+	heading4Node
+	heading5Node
+	heading6Node
+	paragraphNode
+	lineBreakNode
+	escapeNode
+	noWikiNode
+	noWikiInlineNode
+	placeholderNode
+	imageNode
+	linkNode
+	horizontalLineNode
+)
+
 type node struct {
 	nodeType
 
 	parent   *node
 	children []*node
+	value    string
 }
 
 type document struct {
 	root *node
+}
+
+type visitor struct {
+	enter func(*node) error
+	leave func(*node) error
+}
+
+func (d *document) traverse(visitors map[nodeType]*visitor) error {
+
+	if d == nil {
+		return nil
+	}
+
+	var decent func(*node) error
+	decent = func(n *node) error {
+		if n == nil {
+			return nil
+		}
+		v := visitors[n.nodeType]
+		if v != nil && v.enter != nil {
+			if err := v.enter(n); err != nil {
+				return err
+			}
+		}
+
+		for _, child := range n.children {
+			if err := decent(child); err != nil {
+				return err
+			}
+		}
+
+		if v != nil && v.leave != nil {
+			if err := v.leave(n); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	return decent(d.root)
 }
--- xhtml.go
+++ xhtml.go
@@ -3,9 +3,72 @@
 // Copyright 2017 by Intevation GmbH
 package main
 
-import "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+type xhtml struct {
+	out bytes.Buffer
+}
+
+func (x *xhtml) italicsEnter(*node) error {
+	x.out.WriteString("<i>")
+	return nil
+}
+
+func (x *xhtml) italicsLeave(*node) error {
+	x.out.WriteString("</i>")
+	return nil
+}
+
+func (x *xhtml) boldEnter(*node) error {
+	x.out.WriteString("<strong>")
+	return nil
+}
+
+func (x *xhtml) boldLeave(*node) error {
+	x.out.WriteString("</strong>")
+	return nil
+}
+
+func (x *xhtml) horizontalLine(*node) error {
+	x.out.WriteString("<hr/>")
+	return nil
+}
+
+func (x *xhtml) heading(level int) *visitor {
+	return &visitor{
+		enter: func(*node) error {
+			fmt.Fprintf(&x.out, "<h%d>", level)
+			return nil
+		},
+		leave: func(*node) error {
+			fmt.Fprintf(&x.out, "</h%d>", level)
+			return nil
+		},
+	}
+}
 
 func exportXHTML(doc *document, out io.Writer) error {
 	// TODO: Implement me!
-	return nil
+
+	var x xhtml
+	err := doc.traverse(map[nodeType]*visitor{
+		italicsNode:        &visitor{x.italicsEnter, x.italicsLeave},
+		boldNode:           &visitor{x.boldEnter, x.boldLeave},
+		horizontalLineNode: &visitor{leave: x.horizontalLine},
+		heading1Node:       x.heading(1),
+		heading2Node:       x.heading(2),
+		heading3Node:       x.heading(3),
+		heading4Node:       x.heading(4),
+		heading5Node:       x.heading(5),
+		heading6Node:       x.heading(6),
+	})
+	if err != nil {
+		return err
+	}
+	_, err = x.out.WriteTo(out)
+	return err
 }

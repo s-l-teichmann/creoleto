5f80ddcc8101bdbf698aa7e436d256b67ccf0ee6
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2017-09-12 21:37:22 +0200

    Throw away lexer stub.

--- parser.go
+++ parser.go
@@ -3,32 +3,13 @@
 // Copyright 2017 by Intevation GmbH
 package main
 
-import "runtime"
-
 type parser struct {
-	lexer *lexer
 }
 
 func newParser() *parser {
 	return &parser{}
 }
 
-func (p *parser) recover(err *error) {
-	e := recover()
-	if e == nil {
-		return
-	}
-	if _, ok := e.(runtime.Error); ok {
-		panic(e)
-	}
-	if p != nil {
-		p.lexer.drain()
-	}
-	*err = e.(error)
-}
-
-func (p *parser) parse(input string) (doc *document, err error) {
-	p.lexer = newLexer(input)
-	defer p.recover(&err)
-	return
+func (p *parser) parse(input string) (*document, error) {
+	return nil, nil
 }

9deca19b02f1f29943d8f9751269ac4639438c3c
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2017-09-13 23:00:24 +0200

    Removed PITA regexp based parser stuff.

--- parser.go
+++ parser.go
@@ -3,11 +3,6 @@
 // Copyright 2017 by Intevation GmbH
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 type parser struct {
 }
 
@@ -15,25 +10,6 @@ func newParser() *parser {
 	return &parser{}
 }
 
-func preprocess(s string) string {
-	s = strings.Replace(s, "\r\n", "\n", -1)
-	s = strings.Replace(s, "\r", "\n", -1)
-	return s
-}
-
 func (p *parser) parse(input string) (*document, error) {
-	input = preprocess(input)
-	p.parseBlock(input)
 	return nil, nil
-}
-
-func (p *parser) parseBlock(input string) {
-	res := match(blockRE, input)
-	for _, r := range res {
-		r.found(func(k, v string) {
-			fmt.Printf("\t%s: '%s'\n", k, v)
-		})
-		fmt.Println("-------------")
-	}
-
 }
